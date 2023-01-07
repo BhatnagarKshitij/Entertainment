e41737f6d6244093f97a771356bf6f039f8e5433
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2023-01-07 23:59:37 +0530

    Delete Valentine directory

--- Valentine/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-import py2exe
-import ctypes
-
-setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': "main.py"}],
-    zipfile = None,
-)
